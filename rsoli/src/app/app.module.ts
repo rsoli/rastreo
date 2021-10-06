b5072c535a08a5d4f0353b6bb79407432ac65992
Juan Dionicio Jimenez Alcon <juan.jimenez@tde.com.bo>
2021-10-05 22:17:52 -0400

    Correccion de brosser animation module

--- rsoli/src/app/app.module.ts
+++ rsoli/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { SharedModule } from './shared/shared.module';
     HttpClientModule,
     AppRoutingModule,
     SharedModule,
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

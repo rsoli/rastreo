da8fd2d0e68394d39ddcad407ce190373b5cc5cc
Juan Dionicio Jimenez Alcon <juan.jimenez@tde.com.bo>
2021-11-06 22:54:30 -0400

    Controlador para completar ubicaciones faltantes de positions rastreo

--- rastreo/routes/web.php
+++ rastreo/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UbicacionesFaltantesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +21,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('ubicaciones_faltantes', [UbicacionesFaltantesController::class, 'ubicaciones_faltantes']);
+
+

f9160d4cfe64ecff142c0d0f1c5c1e6ed2c5c78b
Juan Dionicio Jimenez Alcon <juan.jimenez@tde.com.bo>
2022-06-05 19:39:17 -0400

    Geocercas bachend  correccion de id geofence

--- rastreo/app/Http/Controllers/ServicioController.php
+++ rastreo/app/Http/Controllers/ServicioController.php
@@ -183,7 +183,7 @@ class ServicioController extends Controller
         if($request->id==0){
             DB::insert('insert into public.tc_geofences (name,description,area) values(?,?,?);',[$request->nombre_geocerca,$request->descripcion," "]); 
             $id_geocerca=DB::select('select max(g.id)::integer as id_geocerca from public.tc_geofences g');
-            DB::insert('insert into public.tc_user_geofence (userid,geofenceid) values(1,?);',[$request->id_geocerca]); 
+            DB::insert('insert into public.tc_user_geofence (userid,geofenceid) values(1,?);',[$id_geocerca]); 
             DB::insert('insert into ras.tusuario_geocerca (id_usuario,id_geocerca) values(?,?);',[($request->user()->id),$id_geocerca]);     
         }
         else{

f40d1fd422c651760bf32bad5e357b40b1cea9ae
root <root@vmi971264.contaboserver.net>
2022-09-03 21:28:05 -0400

    Cambio de dominio

--- rsoli/src/environments/environment.prod.ts
+++ rsoli/src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: "http://154.53.46.118/rastreo/rastreo/public/api"
+
+  //apiUrl: "http://kolosu.com/rastreo/rastreo/public/api"
   //apiUrl: "http://134.122.21.117:81/api"
   // apiUrl: "http://localhost/rastreo/rastreo/public/api"
+  //apiUrl: "http://kolosu.com/rastreo/rastreo/public/api"
+  apiUrl: "https://www.kolosu.com/public/api"
 };
--- rsoli/src/environments/environment.ts
+++ rsoli/src/environments/environment.ts
@@ -10,7 +10,8 @@ export const environment = {
 
   //apiUrl: "http://134.122.21.117:81/api"
   //apiUrl: "https://rsoli.com/rastreo/rastreo/public/api"
-  apiUrl: "http://154.53.46.118/rastreo/rastreo/public/api"
+  //apiUrl: "http://kolosu.com/rastreo/rastreo/public/api"
+  apiUrl: "https://www.kolosu.com/public/api"
 };
 
 /*

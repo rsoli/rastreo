34cab13f08d6016915db3b998b7a826f95648c62
jjimenezetr <juan.jimenez@endetransmision.bo>
2024-10-29 03:03:03 -0400

    cambio de diseño publico

--- kolosu/src/app/demo/components/public/contacto/contacto.component.ts
+++ kolosu/src/app/demo/components/public/contacto/contacto.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.scss']
 })
 export class ContactoComponent {
-
+  openWhatsApp() {
+    const phoneNumber = '59172276601'; // Cambia esto por el número de teléfono deseado
+    const message = 'Hola, me gustaría más información.'; // Mensaje predeterminado
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.open(url, '_blank');
+  }
+  openLink(url: string) {
+    window.open(url, '_blank');
+  }
 }
